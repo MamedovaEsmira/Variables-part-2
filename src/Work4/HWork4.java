6ae0ba2d502a075c970f9f83e4df7da7db5fb32d
mamedovaesmira <mamedovaesmira88@gmail.com>
2022-11-12 23:27:41 +0300

    ДЗ Доделано

--- src/Work4/HWork4.java
+++ src/Work4/HWork4.java
@@ -2,8 +2,7 @@ package Work4;
 
 public class HWork4 {
     public static void main(String[] args) {
-    //Задача1
-    System.out.println("Задача1");
+    System.out.println("Задача 1");
     int plumsCount=3;
     System.out.println("Значение переменной plumsCount с типом int равно "+plumsCount);
     byte dogsCount=6;
@@ -14,10 +13,9 @@ public class HWork4 {
     System.out.println("Значение переменной fishCount с типом long равно "+fishCount);
     float pi=3.14f;
     System.out.println("Значение переменной pi с типом float равно "+pi);
-    double e=2.8;
+    double e =2.8;
     System.out.println("Значение переменной e с типом doubl равно "+e);
-     //Задача2
-        System.out.println("Задача2");
+        System.out.println("Задача 2");
         float variable1=27.12f;
         long variable2=987_678_965_549L;
         double variable3=2.786;
@@ -26,69 +24,89 @@ public class HWork4 {
         int variable6=-159;
         char variable7=27897;
         byte variable8=67;
-        //Задача3
+        System.out.println("Задача 3");
         int pupilsInFirstClass=23;
         int pupilsInSecondClass=27;
         int pupilsInThirdClass=30;
         int papersCount=480;
-        int PapersPerPuls=papersCount/(pupilsInFirstClass+pupilsInSecondClass+pupilsInThirdClass);
-        System.out.println( "На каждого ученика рассчитано"  +PapersPerPuls+"  листов бумаги");
-        //Задача4
+        int papersPerPuls=papersCount/(pupilsInFirstClass+pupilsInSecondClass+pupilsInThirdClass);
+        System.out.println( "На каждого ученика рассчитано "  +papersPerPuls+"  листов бумаги");
+        System.out.println("Задача 4");
         int bottles=16;
         int minutes =2;
-        int ProduktivityPerMinute=bottles/minutes;
-        int TwentyMinutes=20;
-        System.out.println("За"+TwentyMinutes +" минут машина произвела бутылок" +ProduktivityPerMinute*TwentyMinutes + "штук ");
+        int produktivityPerMinute=bottles/minutes;
+        int twentyMinutes=20;
+        System.out.println("За"+twentyMinutes +" минут машина произвела бутылок " +produktivityPerMinute*twentyMinutes + " штук ");
         int minutesInDay=24*60;
-        System.out.println("За"+minutesInDay +"минут машина произвела бутылок" +ProduktivityPerMinute*minutesInDay + "штук ");
+        System.out.println("За"+minutesInDay +"минут машина произвела бутылок " +produktivityPerMinute*minutesInDay + " штук ");
         int minutesInThreeDay=3*minutesInDay;
-        System.out.println("За"+minutesInThreeDay +" минут машина произвела бутылок" +ProduktivityPerMinute*minutesInThreeDay + "штук ");
+        System.out.println("За"+minutesInThreeDay +" минут машина произвела бутылок " +produktivityPerMinute *minutesInThreeDay + " штук ");
         int minutesInMonth=30*minutesInDay;
-        System.out.println("За"+minutesInMonth +" минут машина произвела бутылок" +ProduktivityPerMinute*minutesInMonth + "штук ");
-        //Задача5
-        System.out.println("Задача5");
-        int TotalNumberOfCans=120;
+        System.out.println("За "+minutesInMonth +" минут машина произвела бутылок " +produktivityPerMinute*minutesInMonth + " штук ");
+        System.out.println("Задача 5");
+        int totalNumberOfCans=120;
         byte whitePaint=2;
-        byte BrownPaint=4;
-        int NumberOfCansPerClass=TotalNumberOfCans/(whitePaint+BrownPaint);
-        int TotalWhitePaint=whitePaint*NumberOfCansPerClass;
-        int TotalBrownPaint= BrownPaint* NumberOfCansPerClass;
-        System.out.println("В школе, где "+NumberOfCansPerClass+" классов, нужно "+TotalWhitePaint + " банок белой краски и"+TotalBrownPaint + " банок коричневой краски");
-        //Задача6
-        System.out.println("Задача6");
+        byte brownPaint=4;
+        int numberOfCansPerClass=totalNumberOfCans/(whitePaint+brownPaint);
+        int totalWhitePaint=whitePaint*numberOfCansPerClass;
+        int totalBrownPaint= brownPaint* numberOfCansPerClass;
+        System.out.println("В школе, где "+numberOfCansPerClass+" классов, нужно "+totalWhitePaint + " банок белой краски и"+totalBrownPaint + " банок коричневой краски");
+        System.out.println("Задача 6");
         byte bananes=5;
         int bananaWeight=bananes*80;
         double milk=200;
         double milkWeght=milk/105F;
-        int IceCream=2*100;
+        int iceCream=2*100;
         int eggs=4;
         int eggsWeght=eggs*70;
-        double TotalWeght=bananaWeight+milkWeght+IceCream+eggsWeght;
-        double TotalWeghtKg=TotalWeght/1000;
-        System.out.println( "Общий вес продуктов составляет"+TotalWeghtKg+" кг");
-        //Задача7
-        System.out.println("Задача7");
-        int LoseWeightKg=7;
-        int LoseWeightGr=7*1000;
-        int LoseWeightDayFirst=250;
-        int  LoseWeightDaySecond =500;
-        double FirstWay=LoseWeightGr/250;
-        double SecondDay=LoseWeightGr/500;
-        System.out.println(""+FirstWay+" дней уйдёт на похудение, если спортсмен будет терять каждый день по 250 грамм");
-;       System.out.println(""+SecondDay+" дней уйдёт на похудение, если спортсмен будет терять каждый день по 500 грамм");
-         //Задача8
-         int MashaCurrentSalary=67760;
-         int DenisCurrentSalary=83690;
-         int KristinaCurrentSalary=76230;
-         double MashaIncreasedSalary=MashaCurrentSalary*1.1;
-         double DenisIncreasedSalary=DenisCurrentSalary*1.1F;
-         double KristinaIncreasedSalary=KristinaCurrentSalary*1.1;
-         double MahsaAnnualSalary=(MashaIncreasedSalary-MashaCurrentSalary)*12;
-         double DenisAnnualSalary=(DenisIncreasedSalary-DenisCurrentSalary)*12;
-         double KristinaAnnualSalary=(KristinaIncreasedSalary-KristinaCurrentSalary)*12;
-         System.out.println( "Маша теперь получает" +MashaIncreasedSalary+" рублей. Годовой доход вырос на"+MahsaAnnualSalary+" рублей");
-         System.out.printf( "Денис теперь получает" +DenisIncreasedSalary+" рублей. Годовой доход вырос на"+DenisAnnualSalary+" рублей");
-         System.out.println( "Кристина теперь получает" +KristinaIncreasedSalary+" рублей. Годовой доход вырос на"+KristinaAnnualSalary+" рублей");
+        double totalWeght=bananaWeight+milkWeght+iceCream+eggsWeght;
+        double totalWeghtKg=totalWeght/1000;
+        System.out.printf("Общий вес продуктов составляет:  %.2f\n",  totalWeghtKg );
+        System.out.println("Задача 7");
+        int loseWeightKg=7;
+        int loseWeightGr=loseWeightKg*1000;
+        int loseWeightDayFirst=250;
+        int  loseWeightDaySecond =500;
+        double firstWay=loseWeightGr/loseWeightDayFirst;
+        double secondDay=loseWeightGr/loseWeightDaySecond;
+        System.out.println(""+firstWay+" дней уйдёт на похудение, если спортсмен будет терять каждый день по 250 грамм");
+;       System.out.println(""+secondDay+" дней уйдёт на похудение, если спортсмен будет терять каждый день по 500 грамм");
+        System.out.println("Задача 8");
+         int mashaCurrentSalary=67760;
+         int denisCurrentSalary=83690;
+         int kristinaCurrentSalary=76230;
+         double mashaIncreasedSalary=mashaCurrentSalary*1.1;
+         double denisIncreasedSalary=denisCurrentSalary*1.1F;
+         double kristinaIncreasedSalary=kristinaCurrentSalary*1.1;
+         double mahsaAnnualSalary=(mashaIncreasedSalary-mashaCurrentSalary)*12;
+         double denisAnnualSalary=(denisIncreasedSalary-denisCurrentSalary)*12;
+         double kristinaAnnualSalary=(kristinaIncreasedSalary-kristinaCurrentSalary)*12;
+         System.out.println( "Маша теперь получает " +mashaIncreasedSalary+" рублей. Годовой доход вырос на "+mahsaAnnualSalary+" рублей");
+         System.out.printf( "Денис теперь получает " +denisIncreasedSalary+" рублей. Годовой доход вырос на "+denisAnnualSalary+" рублей");
+         System.out.println( "Кристина теперь получает " +kristinaIncreasedSalary+" рублей. Годовой доход вырос на "+kristinaAnnualSalary+" рублей");
+        System.out.println("Задачи повышенной сложности");
+        System.out.println("Задача 9");
+      byte a = 12;
+      byte b = 27;
+      byte c = 44;
+      byte d = 15;
+      byte e1 = 9;
+      int result = a * (b + (c - d * e1));
+        System.out.println(result);
+        int result1 = - (a * (b + (c - d * e1)));
+        System.out.println(result1);
+        System.out.println("Задача 10");
+        int a1 =5;
+        int b1=7;
+        System.out.printf("a=%d b=%d\n",a1,b1);
+        a1=a1+b1;
+        b1=a1-b1;
+        a1=a1-b1;
+        System.out.printf("a=%d b=%d\n",a1,b1);
+        System.out.println("Задача 11");
+        int a2 = 396;
+        int b2 = a2 % 100/10;
+        System.out.println(b2);
 
     }
 }
